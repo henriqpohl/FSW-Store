627fad9f765c1d4c4b80f2c0f7be2c72dc24cea7
Henrique R. Pohl <henriqpohl@gmail.com>
2023-10-31 12:23:41 +0000

    feat: ensure product can be removed from cart.

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -9,8 +9,11 @@ interface CartItemProps {
 }
 
 const CartItem = ({ product }: CartItemProps) => {
-  const { decreaseProductQuantity, increaseProductQuantity } =
-    useContext(CartContext);
+  const {
+    decreaseProductQuantity,
+    increaseProductQuantity,
+    removeProductFromCart,
+  } = useContext(CartContext);
 
   const handleDecreaseProductQuantityClick = () => {
     decreaseProductQuantity(product.id);
@@ -19,6 +22,11 @@ const CartItem = ({ product }: CartItemProps) => {
   const handleIncreaseProductQuantityClick = () => {
     increaseProductQuantity(product.id);
   };
+
+  const handleRemoveProductClick = () => {
+    alert("asd")
+    removeProductFromCart(product.id)
+  }
 
   return (
     <div className="flex items-center justify-between">
@@ -71,7 +79,11 @@ const CartItem = ({ product }: CartItemProps) => {
       </div>
 
       <div>
-        <Button size="icon" variant="outline">
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={handleRemoveProductClick}
+        >
           <TrashIcon size={16} />
         </Button>
       </div>
